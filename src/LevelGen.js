d536993c408cd22c0ea46266f4575bdabca8fa30
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-06-27 16:34:58 -0700

    Shorten up code some

--- src/LevelGen.js
+++ src/LevelGen.js
@@ -1,28 +1,20 @@
 export class LevelGen {
   // See https://gamedevelopment.tutsplus.com/tutorials/generate-random-cave-levels-using-cellular-automata--gamedev-9664
   static generateCave(cols, rows, {lifeChance = 0.4, birthLimit = 4, deathLimit = 2, steps = 6}) {
-    let cells = Array(cols).fill().map(x => Array(rows).fill(false));
-
-    for (var row = 0; row < rows; row ++) {
-      for (var col = 0; col < cols; col ++) {
-        cells[col][row] = Math.random() < lifeChance;
-      }
-    }
+    let cells = Array.from(
+      Array(cols), () => Array.from(
+        Array(rows), () => Math.random() < lifeChance));
 
     for (let i = 0; i < steps; i ++) {
-      const newCells = Array(cols).fill().map(x => Array(rows).fill(false));
+      const newCells = Array.from(Array(cols), () => new Array(rows));
       
       for (let row = 0; row < rows; row ++) {
         for (let col = 0; col < cols; col ++) {
-  
           // Count nearby walls (out of bounds is also walls)
           let nearby = 8;
           for (let r = Math.max(0, row - 1); r <= Math.min(rows - 1, row + 1); r ++) {
             for (let c = Math.max(0, col - 1); c <= Math.min(cols - 1, col + 1); c ++) {
-              if (r == row && c == col) {
-                // skip
-              }
-              else if (cells[c][r] == false) {
+              if ((r != row || c != col) && !cells[c][r]) {
                 nearby --;
               }
             }
