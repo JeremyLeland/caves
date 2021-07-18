bf327aa73d4e6409a4fd2da02f4ad3b69a0f4a59
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-07-18 13:29:51 -0700

    Cleaned up and sped up height generation code
    Good info here: http://adrianb.io/2014/08/09/perlinnoise.html

--- src/LevelGen.js
+++ src/LevelGen.js
@@ -68,7 +68,27 @@ export class LevelGen {
     return cells;
   }
 
-  static generateHeights({cols, rows, seed = Date.now(), ridged = false}) {
+  static generateLandscapeArray(cols, rows) {
+    const heights = this.generateHeights(cols, rows);
+
+    const terrainVals = [
+      0.41,   // water
+      0.47,   // beach
+      0.58,   // grass
+      0.68,   // mount
+      1.00    // snow!
+    ];
+
+    return Array.from(heights, col => Array.from(col, val => {
+      for (let i = 0; i < terrainVals.length; i ++) {
+        if (val < terrainVals[i]) {
+          return i;
+        }
+      }
+    }));
+  }
+
+  static generateHeights(cols, rows, {ridged = false, seed = Date.now(), octaves = 5, frequency = 0.02, persistance = 0.5} = {}) {
     const timeStr = `Generating ${cols}x${rows} height map with seed ${seed}`;
     console.time(timeStr);
     
@@ -76,12 +96,20 @@ export class LevelGen {
 
     for (var row = 0; row < rows; row ++) {
       for (var col = 0; col < cols; col ++) {
-        var amplitude = 0.5;
+        let total = 0;
+        let freq = frequency;
+        let amplitude = 1;
+        let maxValue = 0;
 
-        for (let i = 1; i <= 5; i++) {
-          heights[col][row] += amplitude * Perlin.noise2(seed + col * Math.pow(2,i) / 200, seed + row * Math.pow(2,i) / 200);
-          amplitude *= 0.5;
+        for (let i = 1; i <= octaves; i++) {
+          total += Perlin.noise2(col * freq, row * freq) * amplitude;
+
+          maxValue += amplitude;
+          amplitude *= persistance;
+          freq *= 2;
         }
+
+        heights[col][row] = total / maxValue;
 
         if (ridged) {
           heights[col][row] = Math.abs(0.5 - heights[col][row]) * 2;
@@ -93,5 +121,3 @@ export class LevelGen {
     return heights;
   }
 }
-
-  
