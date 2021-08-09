815631eb93de51b225b26d216cf89a722d6b84c2
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-08-09 02:57:37 -0700

    Try loading blends from texture instead of generating them ourselves

--- src/BlendTiles.js
+++ src/BlendTiles.js
@@ -140,3 +140,87 @@ export function create( size ) {
   console.timeEnd( timeStr) ;
   return canvas;
 }
+
+const tiles = [
+  // { pattern: Pattern.EMPTY, },   just draw a black square
+      
+  { pattern: Pattern.NW, tile: [ 7, 1 ] },
+  { pattern: Pattern.NE, tile: [ 6, 1 ] },
+  { pattern: Pattern.SW, tile: [ 7, 0 ] },
+  { pattern: Pattern.SE, tile: [ 6, 0 ] },
+
+  { pattern: Pattern.N, tile: [ 0, 0 ] },
+  { pattern: Pattern.W, tile: [ 0, 1 ] },
+  { pattern: Pattern.E, tile: [ 0, 2 ] },
+  { pattern: Pattern.S, tile: [ 0, 3 ] },
+
+  // { pattern: Pattern.NW_SE,      combine NW and SE
+  // { pattern: Pattern.NE_SW,      combine NE and SW
+    
+  { pattern: Pattern.N_W, tile: [ 4, 0 ] },
+  { pattern: Pattern.N_E, tile: [ 5, 0 ] },
+  { pattern: Pattern.S_W, tile: [ 4, 1 ] },
+  { pattern: Pattern.S_E, tile: [ 5, 1 ] },
+
+  // { pattern: Pattern.SOLID,      just draw a white square
+];
+
+export async function load( path ) {
+  const timeStr = `Loading and arranging blend tiles from ${path}`;
+  console.time( timeStr );
+
+  const src = new Image();
+  src.src = path;
+  await src.decode();
+
+  const size = 256;
+
+  const canvas = document.createElement( 'canvas' );
+  canvas.width = size;
+  canvas.height = size * 16;
+  const ctx = canvas.getContext( '2d' );
+
+  let y = 0;
+  for (let nw = 0; nw < 2; nw ++) {
+    for (let ne = 0; ne < 2; ne ++) {
+      for (let sw = 0; sw < 2; sw ++) {
+        for (let se = 0; se < 2; se ++) {
+          if ( Pattern.EMPTY( nw, ne, sw, se ) ) {
+            ctx.fillStyle = 'black';
+            ctx.fillRect( 0, y, size, size );
+          }
+          else if ( Pattern.SOLID( nw, ne, sw, se ) ) {
+            ctx.fillStyle = 'white';
+            ctx.fillRect( 0, y, size, size );
+          }
+          else if ( Pattern.NW_SE( nw, ne, sw, se ) ) {
+            drawCorners( Pattern.NW, Pattern.SE );
+          }
+          else if ( Pattern.NE_SW( nw, ne, sw, se ) ) {
+            drawCorners( Pattern.NE, Pattern.SW );
+          }
+          else {
+            const tile = tiles.find(e => e.pattern(nw, ne, sw, se)).tile;
+            ctx.drawImage( src, tile[0] * size, tile[1] * size, size, size, 0, y, size, size );
+          }
+
+          y += size;
+        }
+      }
+    }
+  }
+
+  function drawCorners( pattern1, pattern2 ) {
+    const src1 = tiles.find( e => e.pattern == pattern1 ).tile;
+    const src2 = tiles.find( e => e.pattern == pattern2 ).tile;
+
+    ctx.globalCompositeOperation = 'lighter';
+    ctx.drawImage( src, src1[0] * size, src1[1] * size, size, size, 0, y, size, size );
+    ctx.drawImage( src, src2[0] * size, src2[1] * size, size, size, 0, y, size, size );
+    ctx.globalCompositeOperation = 'source-over';
+
+  }
+
+  console.timeEnd( timeStr) ;
+  return canvas;
+}
