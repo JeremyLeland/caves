4c80cc55eaad3ec46a39faa616103e0fc694a4fd
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-08-16 03:19:26 -0700

    Simplest of UI

--- src/test/MapEditor.ts
+++ src/test/MapEditor.ts
@@ -2,21 +2,23 @@ import { TileInfos, TileMap } from '../TileMap.js';
 
 setGridStyle( document.body, 'rgba(100, 100, 100, .7)' );
 
-const currentGridIndicator = document.createElement( 'div' );
-currentGridIndicator.style.position = 'absolute';
-currentGridIndicator.style.width = '32px';
-currentGridIndicator.style.height = '32px';
-setGridStyle( currentGridIndicator, 'white' );
-document.body.appendChild( currentGridIndicator );
+const gridCursor = getGridCursor();
+document.body.appendChild( gridCursor );
 
-function setGridStyle( element: HTMLElement, color: string ) {
-  element.style.backgroundImage = `
-  linear-gradient(${ color } 1px, transparent 1px, transparent 31px, ${ color } 1px),
-  linear-gradient(90deg, ${ color } 1px, transparent 1px, transparent 31px, ${ color } 1px)
-  `;
-  element.style.backgroundSize = '32px 32px';
-}
+const ui = document.createElement( 'div' );
+ui.style.position = 'absolute';
+ui.style.right = '0';
+document.body.appendChild( ui );
 
+const tileInfos = [ TileInfos.Dirt, TileInfos.Grass, TileInfos.Snow ];
+let activeTileIndex = 1;
+
+tileInfos.forEach( ( tileInfo, index ) => {
+  const button = document.createElement( 'button' );
+  button.innerText = `${index}`;
+  button.onclick = () => activeTileIndex = index;
+  ui.appendChild( button );
+});
 
 let cols = 20, rows = 20;
 let tileIndices = Array( cols * rows ).fill( 0 );
@@ -24,7 +26,7 @@ let tileIndices = Array( cols * rows ).fill( 0 );
 const tileMap = new TileMap( {
   cols: 10, rows: 10,
   tileMap: tileIndices,
-  tiles: [ TileInfos.Dirt, TileInfos.Grass, TileInfos.Snow ],
+  tiles: tileInfos,
 } );
 
 document.body.appendChild( tileMap.canvas );
@@ -42,8 +44,8 @@ window.onmousemove = ( e: MouseEvent ) => {
   mouseCol = Math.floor( e.clientX / tileMap.tileSize );
   mouseRow = Math.floor( e.clientY / tileMap.tileSize );
 
-  currentGridIndicator.style.left = `${mouseCol * tileMap.tileSize}`;
-  currentGridIndicator.style.top  = `${mouseRow * tileMap.tileSize}`;
+  gridCursor.style.left = `${mouseCol * tileMap.tileSize}`;
+  gridCursor.style.top  = `${mouseRow * tileMap.tileSize}`;
 
   if ( mouseDown ) {
     doMouse();
@@ -55,6 +57,23 @@ function doMouse() {
     lastCol = mouseCol;
     lastRow = mouseRow;
 
-    tileMap.setTileAt( mouseCol, mouseRow, 1 );
+    tileMap.setTileAt( mouseCol, mouseRow, activeTileIndex );
   }
+}
+
+function setGridStyle( element: HTMLElement, color: string ) {
+  element.style.backgroundImage = `
+  linear-gradient(${ color } 1px, transparent 1px, transparent 31px, ${ color } 1px),
+  linear-gradient(90deg, ${ color } 1px, transparent 1px, transparent 31px, ${ color } 1px)
+  `;
+  element.style.backgroundSize = '32px 32px';
+}
+
+function getGridCursor() {
+  const div = document.createElement( 'div' );
+  div.style.position = 'absolute';
+  div.style.width = '32px';
+  div.style.height = '32px';
+  setGridStyle( div, 'white' );
+  return div;
 }
