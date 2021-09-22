3c7f1bf94c88cc816beb3ca1af77a30bf870ce59
Jeremy Leland <jeremy.leland@gmail.com>
2021-09-22 10:59:59 -0700

    Move frame updates to actor

--- divtest/actor.js
+++ divtest/actor.js
@@ -1,5 +1,7 @@
 import { TileSize } from './tilemap.js';
 
+const TIME_BETWEEN_FRAMES = 100;
+
 const spriteInfos = await ( await fetch( './spriteInfos.json' ) ).json();
 const actorInfos  = await ( await fetch( './actorInfos.json'  ) ).json();
 
@@ -45,12 +47,20 @@ export function fromJson( json ) {
     speed: actorInfo.speed,
     action: 'walk',
     frame: 0,
+    timers: { frame: 0, wait: 0 },
     spriteInfo: spriteInfos[ actorInfo.spriteInfoKey ],
     div: div
   };
 }
 
 export function update( { actor, others, dt } ) {
+  for ( let t in actor.timers ) {
+    actor.timers[ t ] -= dt;
+  }
+
+
+  // Wait if we are tooClose (don't set back to idle)
+
   const tooClose = others.some( other => {
     const cx = other.x - actor.x;
     const cy = other.y - actor.y;
@@ -62,10 +72,23 @@ export function update( { actor, others, dt } ) {
 
   if ( tooClose ) {
     actor.action = 'idle';
+    actor.frame = 0;
   }
   else {
     actor.action = 'walk';
     updateLocation( actor, dt );
+  }
+
+  // Frame updates
+  if ( actor.timers.frame < 0 ) {
+    actor.timers.frame += TIME_BETWEEN_FRAMES;
+
+    const action = actor.spriteInfo.actions[ actor.action ];
+    actor.frame = ( actor.frame + 1 ) % action.frames;
+
+    if ( actor.frame == 0 && !action.loop ) {
+      actor.action = 'idle';
+    }
   }
 
   updateSprite( actor );
