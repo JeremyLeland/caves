56413674fb6e42c3ff16beb3e500e7220a77c15b
Jeremy Leland <jeremy.leland@gmail.com>
2021-09-25 06:57:39 -0700

    Make Actor an object, since I was basically writing object oriented code anyway.
    At least this is cleaner to call.

--- divtest/actor.js
+++ divtest/actor.js
@@ -7,179 +7,178 @@ const TIME_TO_WAIT = 3000;
 const spriteInfos = await ( await fetch( './spriteInfos.json' ) ).json();
 const actorInfos  = await ( await fetch( './actorInfos.json'  ) ).json();
 
-export function prepareCSS() {
-  const styleSheet = document.styleSheets[ 0 ];
+export class Actor {
+  static prepareCSS() {
+    const styleSheet = document.styleSheets[ 0 ];
 
-  for ( let spriteInfoKey in spriteInfos ) {
-    const spriteInfo = spriteInfos[ spriteInfoKey ];
-    styleSheet.insertRule( `.${ spriteInfoKey } { 
-      width: ${ spriteInfo.width }; 
-      height: ${ spriteInfo.height };
-    }` );
-  }
-
-  for ( let actorInfoKey in actorInfos ) {
-    const actorInfo = actorInfos[ actorInfoKey ];
-    actorInfo.layers.forEach( layer => {
-      styleSheet.insertRule( `.${ actorInfo.spriteInfoKey } > .${ layer } {
-        background-image: url( ${ actorInfo.imagesPath }/${ layer }.png );
+    for ( let spriteInfoKey in spriteInfos ) {
+      const spriteInfo = spriteInfos[ spriteInfoKey ];
+      styleSheet.insertRule( `.${ spriteInfoKey } { 
+        width: ${ spriteInfo.width }; 
+        height: ${ spriteInfo.height };
       }` );
-    } );
+    }
+
+    for ( let actorInfoKey in actorInfos ) {
+      const actorInfo = actorInfos[ actorInfoKey ];
+      actorInfo.layers.forEach( layer => {
+        styleSheet.insertRule( `.${ actorInfo.spriteInfoKey } > .${ layer } {
+          background-image: url( ${ actorInfo.imagesPath }/${ layer }.png );
+        }` );
+      } );
+    }
   }
-}
 
-export function fromJson( json ) {
-  const actorInfo = actorInfos[ json.actorInfoKey ];
+  constructor( json ) {
+    const actorInfo = actorInfos[ json.actorInfoKey ];
 
-  const div = document.createElement( 'div' );
-  div.setAttribute( 'class', `actor ${ actorInfo.spriteInfoKey }` );
-  document.body.appendChild( div );
+    const div = document.createElement( 'div' );
+    div.setAttribute( 'class', `actor ${ actorInfo.spriteInfoKey }` );
+    document.body.appendChild( div );
 
-  actorInfo.layers.forEach ( layer => {
-    const layerDiv = document.createElement( 'div' );
-    layerDiv.setAttribute( 'class', `layer ${ layer }` );
-    div.appendChild( layerDiv );
-  });
+    actorInfo.layers.forEach ( layer => {
+      const layerDiv = document.createElement( 'div' );
+      layerDiv.setAttribute( 'class', `layer ${ layer }` );
+      div.appendChild( layerDiv );
+    });
 
-  return {
-    x: ( json.col + 0.5 ) * TileSize,
-    y: ( json.row + 0.5 ) * TileSize, 
-    team: json.team,
-    angle: Math.PI / 2,
-    life: actorInfo.life,
-    speed: actorInfo.speed,
-    action: 'walk',
-    attack: actorInfo.attack,
-    frame: 0,
-    timers: { frame: 0, wait: 0 },
-    spriteInfo: spriteInfos[ actorInfo.spriteInfoKey ],
-    div: div
-  };
-}
-
-export function setGoalNode( actor, goalNode ) {
-  if ( goalNode ) {
-    actor.path = Pathfinding.getPath( actor.currentNode, goalNode );
-    updatePathSVG( actor );
+    this.x = ( json.col + 0.5 ) * TileSize;
+    this.y = ( json.row + 0.5 ) * TileSize; 
+    this.team = json.team;
+    this.angle = Math.PI / 2;
+    this.life = actorInfo.life;
+    this.speed = actorInfo.speed;
+    this.action = 'walk';
+    this.attack = actorInfo.attack;
+    this.frame = 0;
+    this.timers = { frame: 0, wait: 0 };
+    this.spriteInfo = spriteInfos[ actorInfo.spriteInfoKey ];
+    this.div = div
   }
-}
 
-function updatePathSVG( actor ) {
-  if ( !actor.pathSVG ) {
-    actor.pathSVG = Pathfinding.getPathSVG( actor.path );
-    document.body.appendChild( actor.pathSVG );
+  setGoalNode( goalNode ) {
+    if ( goalNode ) {
+      this.path = Pathfinding.getPath( this.currentNode, goalNode );
+      this.#updatePathSVG();
+    }
   }
-  else {
-    actor.pathSVG.firstChild.setAttribute( 'd', Pathfinding.getPathSVGDString( actor.path ) );
+
+  #updatePathSVG() {
+    if ( !this.pathSVG ) {
+      this.pathSVG = Pathfinding.getPathSVG( this.path );
+      document.body.appendChild( this.pathSVG );
+    }
+    else {
+      this.pathSVG.firstChild.setAttribute( 'd', Pathfinding.getPathSVGDString( this.path ) );
+    }
   }
-}
 
-function distanceBetween( actor, other ) {
-  return Math.hypot( actor.x - other.x, actor.y - other.y );
-}
-
-export function update( { actor, others, dt } ) {
-  if ( actor.timers.wait > 0 ) {
-    actor.timers.wait -= dt;
+  distanceFrom( other ) {
+    return Math.hypot( this.x - other.x, this.y - other.y );
   }
-  else {
-    // Don't try to start an attack or move if we are already attacking (or dying)
-    if ( actor.action == 'idle' || actor.action == 'walk' ) {
-      if ( actor.target && distanceBetween( actor, actor.target ) < TileSize ) {
-        actor.action = actor.attack;
-        actor.frame = 0;
-      }
-      else {
-        // TODO: Wait a bit if we are tooClose (so we aren't twitching so much)
-        const tooClose = false; /*others.some( other => {
-          const cx = other.x - actor.x;
-          const cy = other.y - actor.y;
-          const otherInFront = 0 < cx * Math.cos( actor.angle ) + cy * Math.sin( actor.angle );
-          const distToOther = Math.hypot( actor.x - other.x, actor.y - other.y );
 
-          return otherInFront && distToOther < TileSize;
-        } );*/
+  update( { others, dt } ) {
+    if ( this.timers.wait > 0 ) {
+      this.timers.wait -= dt;
+    }
+    else {
+      // Don't try to start an attack or move if we are already attacking (or dying)
+      if ( this.action == 'idle' || this.action == 'walk' ) {
+        if ( this.target && this.distanceFrom( this.target ) < TileSize ) {
+          this.action = this.attack;
+          this.frame = 0;
+        }
+        else {
+          // TODO: Wait a bit if we are tooClose (so we aren't twitching so much)
+          const tooClose = false; /*others.some( other => {
+            const cx = other.x - this.x;
+            const cy = other.y - this.y;
+            const otherInFront = 0 < cx * Math.cos( this.angle ) + cy * Math.sin( this.angle );
+            const distToOther = Math.hypot( this.x - other.x, this.y - other.y );
 
-        if ( !tooClose ) {
-          doMove( actor, dt );
+            return otherInFront && distToOther < TileSize;
+          } );*/
 
-          if ( actor.path?.length > 0 ) {
-            actor.action = 'walk';
-          }
-          else {
-            actor.path = null;
-            actor.action = 'idle';
-            actor.frame = 0;
-            actor.timers.wait = actor.target ? 0 : TIME_TO_WAIT;
+          if ( !tooClose ) {
+            this.#doMove( dt );
+
+            if ( this.path?.length > 0 ) {
+              this.action = 'walk';
+            }
+            else {
+              this.path = null;
+              this.action = 'idle';
+              this.frame = 0;
+              this.timers.wait = this.target ? 0 : TIME_TO_WAIT;
+            }
           }
         }
       }
+
+      this.#updateFrame( dt );
     }
 
-    updateFrame( actor, dt );
+    this.#updateSprite();
   }
 
-  updateSprite( actor );
-}
+  #doMove( dt ) {
+    let moveDist = this.speed * dt;
 
-function doMove( actor, dt ) {
-  let moveDist = actor.speed * dt;
+    while ( moveDist > 0 && this.path?.length > 0 ) {
+      const waypoint = this.path[ 0 ];
+      const distToWaypoint = Math.hypot( waypoint.x - this.x, waypoint.y - this.y );
 
-  while ( moveDist > 0 && actor.path?.length > 0 ) {
-    const waypoint = actor.path[ 0 ];
-    const distToWaypoint = Math.hypot( waypoint.x - actor.x, waypoint.y - actor.y );
+      if ( distToWaypoint < moveDist ) {
+        this.x = waypoint.x;
+        this.y = waypoint.y;
+        this.currentNode = this.path.shift();
+        this.#updatePathSVG();
+      }
+      else {
+        this.angle = Math.atan2( waypoint.y - this.y, waypoint.x - this.x );
+        this.x += Math.cos( this.angle ) * moveDist;
+        this.y += Math.sin( this.angle ) * moveDist;
+      }
 
-    if ( distToWaypoint < moveDist ) {
-      actor.x = waypoint.x;
-      actor.y = waypoint.y;
-      actor.currentNode = actor.path.shift();
-      updatePathSVG( actor );
-    }
-    else {
-      actor.angle = Math.atan2( waypoint.y - actor.y, waypoint.x - actor.x );
-      actor.x += Math.cos( actor.angle ) * moveDist;
-      actor.y += Math.sin( actor.angle ) * moveDist;
-    }
-
-    moveDist -= distToWaypoint;
-  }
-}
-
-function updateFrame( actor, dt ) {
-  actor.timers.frame -= dt;
-
-  if ( actor.timers.frame < 0 ) {
-    actor.timers.frame += TIME_BETWEEN_FRAMES;
-
-    const action = actor.spriteInfo.actions[ actor.action ];
-    actor.frame = ( actor.frame + 1 ) % action.frames;
-
-    if ( actor.frame == 0 && !action.loop ) {
-      actor.action = 'idle';
+      moveDist -= distToWaypoint;
     }
   }
-}
 
+  #updateFrame( dt ) {
+    this.timers.frame -= dt;
 
-function updateSprite( actor ) {
-  const spriteInfo = actor.spriteInfo;
+    if ( this.timers.frame < 0 ) {
+      this.timers.frame += TIME_BETWEEN_FRAMES;
 
-  const dir = actor.action == 'die' ? 'south' : directionFromAngle( actor.angle );
+      const action = this.spriteInfo.actions[ this.action ];
+      this.frame = ( this.frame + 1 ) % action.frames;
 
-  const action = spriteInfo.actions[ actor.action ];
+      if ( this.frame == 0 && !action.loop ) {
+        this.action = 'idle';
+      }
+    }
+  }
 
-  const center = spriteInfo.centers ? spriteInfo.centers[ dir ] : spriteInfo.center;
-  const col = action.col + actor.frame;
+  #updateSprite() {
+    const spriteInfo = this.spriteInfo;
 
-  const dirIndex = spriteInfo.dirIndex[ dir ];
-  const row = action.row + dirIndex;
+    const dir = this.action == 'die' ? 'south' : directionFromAngle( this.angle );
 
-  const style = actor.div.style;
-  const x = Math.floor( actor.x - center.x );
-  const y = Math.floor( actor.y - center.y );
-  style.transform = `translate( ${ x }px, ${ y }px )`;
-  style.zIndex = Math.floor( actor.y );
-  style.backgroundPosition = `-${ col * 100 }% -${ row * 100 }%`;
+    const action = spriteInfo.actions[ this.action ];
+
+    const center = spriteInfo.centers ? spriteInfo.centers[ dir ] : spriteInfo.center;
+    const col = action.col + this.frame;
+
+    const dirIndex = spriteInfo.dirIndex[ dir ];
+    const row = action.row + dirIndex;
+
+    const style = this.div.style;
+    const x = Math.floor( this.x - center.x );
+    const y = Math.floor( this.y - center.y );
+    style.transform = `translate( ${ x }px, ${ y }px )`;
+    style.zIndex = Math.floor( this.y );
+    style.backgroundPosition = `-${ col * 100 }% -${ row * 100 }%`;
+  }
 }
 
 function directionFromAngle( angle ) {
