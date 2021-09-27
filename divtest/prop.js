3b1eb036669247887438691b0b91ab719bc29cdc
Jeremy Leland <jeremy.leland@gmail.com>
2021-09-27 08:02:01 -0700

    Added some bridges
    Realized I wasn't actually taking into account propInfo.passable
    
    Returning it from Prop.fromJson for now. The fromJson functions are not very consistent right now, should think on this more.

--- divtest/prop.js
+++ divtest/prop.js
@@ -9,8 +9,8 @@ export function prepareCSS() {
     const propInfo = propInfos[ propInfoKey ];
     styleSheet.insertRule( `.${ propInfoKey } { 
       background-image: url( ${ propInfo.src.path } );
-      width: ${ propInfo.size.cols * TileSize }; 
-      height: ${ propInfo.size.rows * TileSize };
+      width: ${ ( propInfo.size?.cols ?? 1 ) * TileSize }; 
+      height: ${ ( propInfo.size?.rows ?? 1 ) * TileSize };
       background-position: -${ propInfo.src.col * TileSize } -${ propInfo.src.row * TileSize };
     }` );
   }
@@ -22,8 +22,8 @@ export function fromJson( json ) {
   const div = document.createElement( 'div' );
   div.setAttribute( 'class', `prop ${ json.propInfoKey }` );
 
-  const x = ( json.col - propInfo.offset.cols ) * TileSize;
-  const y = ( json.row - propInfo.offset.rows ) * TileSize;
+  const x = ( json.col - ( propInfo.offset?.cols ?? 0 ) ) * TileSize;
+  const y = ( json.row - ( propInfo.offset?.rows ?? 0 ) ) * TileSize;
 
   const style = div.style;
   style.transform = `translate( ${ x }px, ${ y }px )`;
@@ -31,4 +31,7 @@ export function fromJson( json ) {
 
   // TODO: don't add directly to document.body, return div so they can be added as batch?
   document.body.appendChild( div );
+
+  // TODO: these fromJson function should return something more consistent
+  return propInfo;
 }
